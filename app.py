23481b66d96fdc38b4a2f23dcd58136f21746745
EdwardCrazyThurs <hxdl0325@icloud.com>
2025-02-07 12:45:58 +0000

    Update sth from TT

--- app.py
+++ app.py
@@ -15,37 +15,37 @@ app = Flask("__name__")
 
 @app.route("/", methods = ["GET","POST"])
 def index():
-    return(render_template("index2.html"))
+    return(render_template("index2.html"))
 
 @app.route("/main", methods = ["GET","POST"])
 def main():
-    name = request.form.get('q')
-    return(render_template("main.html"))
+    name = request.form.get('q')
+    return(render_template("main.html"))
 
 @app.route("/SA", methods = ["GET","POST"])
 def SA():
-    return(render_template("SA.html"))
+    return(render_template("SA.html"))
 
 @app.route("/SA_result", methods = ["GET","POST"])
 def SA_result():
-    q = request.form.get('q')
-    r = textblob.TextBlob(q).sentiment
-    return(render_template("SA_result.html",r = r))
+    q = request.form.get('q')
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html",r = r))
 
 @app.route("/paynow", methods = ["GET","POST"])
-def GenAI():
-    return(render_template("GenAI.html"))
+def Paynow():
+    return(render_template("paynow.html"))
 
 @app.route("/GenAI", methods = ["GET","POST"])
 def GenAI():
-    return(render_template("paynow.html"))
+    return(render_template("genAI.html"))
 
 @app.route("/GenAI_result", methods = ["GET","POST"])
 def GenAI_result():
-    q = request.form.get('q')
-    r = model.generate_content(q)
-    return(render_template("GenAI_result.html",r = r.candidates[0].content.parts[0].text))
+    q = request.form.get('q')
+    r = model.generate_content(q)
+    return(render_template("genAI_result.html",r = r.candidates[0].content.parts[0].text))
 
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(debug= True)
 
